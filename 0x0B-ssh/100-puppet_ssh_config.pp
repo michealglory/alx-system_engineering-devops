2f19cc8da6a11ae41aa785a7b0b502aeb85b9c88
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2024-01-05 03:42:40 +0100

    ensure puppet resource file exist

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,7 @@
 # Puppet script to create ssh config file
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
