99d164247858d7caf74a06c60ee0f73d5870f9d3
Michael Glory M <133113993+michealglory@users.noreply.github.com>
2024-02-23 05:54:18 +0100

    Modify nginx to allow more concurrent connection

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Modify nginx to allow more concurrent connection
+
+# Increase the open file descriptor limit
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
